d728bfe563800ce8334016565ed17c48da20305c
OpenJC <openjc@Mac.fritz.box>
2026-05-24 17:24:02 +0200

    Improve vendor card action icon

--- wp-content/themes/wichtelwerken-child/functions.php
+++ wp-content/themes/wichtelwerken-child/functions.php
@@ -21,7 +21,7 @@ function ww_enqueue_assets() {
         'wichtelwerken-child',
         get_stylesheet_uri(),
         ['storefront-parent'],
-        '2.0.3'
+        '2.0.5'
     );
     wp_enqueue_script(
         'wichtelwerken-main',
